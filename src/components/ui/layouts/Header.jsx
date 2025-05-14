45a7f2ac441821c57ad6f8a658c9e5940d9f657d
meesha.uwu <trann8202@gmail.com>
2025-05-14 08:04:54 +0700

    update Header

--- src/components/ui/layouts/Header.jsx
+++ src/components/ui/layouts/Header.jsx
@@ -1,33 +1,30 @@
-import { Button, Row } from 'antd'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Header.css";
 
-const Header = () => {
-    return (
-        <Row className='py-2'>
-            <div className='container flex items-center'>
-                <img
-                    className='h-[55px] w-[80px] object-cover'
-                    src="images/logos/logo-estate.png"
-                    alt="Logo"
-                />
-                <ul className='flex ml-20'>
-                    <li className='text-[18px]  px-[12px] py-2 '>
-                        <Link className='font-[Roboto]'>Trang chủ</Link>
-                    </li>
-                    <li className='text-[18px] font-medium px-[12px] py-2 '>
-                        <Link className=''>Dịch vụ</Link>
-                    </li>
-                    <li className='text-[18px] font-medium px-[12px] py-2'>
-                        <Link>Thông tin</Link>
-                    </li>
-                </ul>
-                <Button className='ml-auto text-white font-medium px-4 py-2 rounded-[8px]'>
-                    Đăng Nhập
-                </Button>
-            </div>
-        </Row>
-    )
-}
+const Header = () => (
+  <header className="header">
+    <div className="header-top">
+      <div className="logo">
+        <img src="/images/logos/logo-estate.png" alt="Modern Estate" />
+      </div>
+      <nav className="main-nav">
+        <a className="active" href="#">Trang chủ</a>
+        <a href="#">Dịch vụ</a>
+        <a href="#">Thông tin</a>
+      </nav>
+      <button className="login-btn">Đăng nhập</button>
+    </div>
+    <div className="header-bottom">
+      <nav className="sub-nav">
+        <a href="#">MUA BÁN CĂN HỘ</a>
+        <a href="#">CHO THUÊ CĂN HỘ</a>
+        <a href="#">DỰ ÁN</a>
+        <a href="#">GIỚI THIỆU</a>
+        <a href="#">PHÂN TÍCH ĐÁNH GIÁ</a>
+        <a href="#">BLOG</a>
+      </nav>
+    </div>
+  </header>
+);
 
-export default Header
+export default Header;
